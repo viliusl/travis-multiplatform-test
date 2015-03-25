3407d3cbbefec56d04feb59e0c1985a1947c5a6b
Vilius Lukosius <vilius@wix.com>
2015-03-25 23:55:10 +0200

    Added initial project config

---
+++ src/test/scala/com/mycompany/app/SampleTest.scala
@@ -0,0 +1,15 @@
+package com.mycompany.app
+
+import org.specs2.mutable.SpecificationWithJUnit
+
+/**
+ * @author viliusl
+ * @since 25/03/15
+ */
+class SampleTest extends SpecificationWithJUnit {
+  "this test" should {
+    "pass" in {
+      success
+    }
+  }
+}
